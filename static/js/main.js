953e9487bec0204754f3025dedf19099fae41415
raphael <raphael@dexterity.ai>
2020-04-25 14:15:55 -0700

    version 2 with some basic videos

--- static/js/main.js
+++ static/js/main.js
@@ -3,12 +3,30 @@ var counter = 0;
 $(document).ready(function(){
 
     //console.log(jsonObject);
-    fetch('./static//data/workout1.json')
+    fetch('./static/data/workout1.json')
         .then((response) => {
             return response.json();
         })
         .then((data) => {
-            console.log(data);
+
+            let searchParams = new URLSearchParams(window.location.search)
+            if(searchParams.has('timestamp')) {
+                let timestamp = searchParams.get('timestamp')
+
+                var wait = {
+                    "id": 0,
+                    "heading": "Wait for Group Session",
+                    "name": "Session will start soon",
+                    "duration": 10,
+                    "gifpath": "static/movie/Crunches.mp4"
+};
+                console.log("Timestamp found!!")
+                data.startTime = dayjs(timestamp);
+                data.elements.unshift(wait);
+            }else{
+                data.startTime = 'now';
+            }
+
             createCarousel(data);
             parseResults(data);
 
@@ -29,13 +47,13 @@ $(document).ready(function(){
 
 function createCarousel(data) {
     $.each (data['elements'], function(index,elem) {
-        $('<div class="carousel-item"><h2>'+elem.name+'</h2>' +
-            '<video onloadeddata="this.play();"  width="320" height="240" playsinline loop muted preload>\n' +
-            '    <source src="./static//movie/test.webm" type="video/webm" />\n' +
+        $('<div class="carousel-item"><h2 id="name-'+elem.id+'">'+elem.name+'</h2>' +
+            '<video onloadeddata="this.play();"  playsinline loop muted preload autoplay>\n' +
+            '    <source src="'+elem.gifpath+'" type="video/mp4" />\n' +
             '    Your browser does not support the video tag or the file format of this video.\n' +
             '</video>'+
             '<div id=timer-'+elem.id+'></div>').appendTo('.carousel-inner');
-        $('<li data-targe="#carousel" data-slide-to="' + index + '"></li>').appendTo('.carousel-indicators');
+        $('<li data-targe="#carousel" data-slide-to="' + elem.id + '"></li>').appendTo('.carousel-indicators');
     })
     // ------------    SHOW CAROUSEL    -------------
     $('#carousel').carousel();
@@ -48,45 +66,45 @@ function createCarousel(data) {
 
 function parseResults(data) {
     var startTime = null;
-    if(data['startTime'] == "now"){
-        startTime = dayjs(Date.now())}
-    else{
-        startTime = dayjs(data['startTime']);
+    if(data.startTime!="now"){
+        startTime = dayjs(data.startTime)
+    }else{
+       startTime = dayjs(Date.now())
     }
     data.elements.sort(function(a, b){
         return a.id - b.id;
     });
+
     var time_list = [startTime]
     var old_time = startTime;
     data['elements'].forEach(function (item, index) {
         item.timeStamp = old_time.add(item.duration,'seconds')
         old_time = item.timeStamp
-        console.log(item)
     });
-    console.log(data);
-
-    console.log(time_list.sort((a, b) => (dayjs(a).isAfter(dayjs(b)) ? 1 : -1)))
 
     startJqueryTimer(data);
 }
 
 function startJqueryTimer(startTime) {
-
-
+    console.log(startTime['elements'].length)
+    if(startTime['elements'].length==0){
+        $("#content").empty()
+        $("#content").html('<h1>!!! Workout over !!!!</h1>')
+        return
+    }
     var element = startTime['elements'].shift()
     $('#heading').text(element.heading);
     var elemId = uniqId()
-    var timer_name = $("<div></div>").text(element.name);
     var timer_gui = $("#timer-"+element.id).text("00:00 ").css('font-size', 'xx-large');
 
-    //console.log("Elements2",(['timeStamp'])
     timer_gui.countdown({
         until: new Date((element['timeStamp'])),
-        compact: true, format: 'MS',
+        compact: true, format: 'DHMS',
         onExpiry: function expired() {
-            $('.carousel').carousel("next");
-            console.log('Bla')
-            startJqueryTimer(startTime)
+            $('#heading').text('-');
+            $(".carousel.active").empty()
+            $('.carousel').carousel('next');
+            startJqueryTimer(startTime);
         },
         alwaysExpire: true
     });
